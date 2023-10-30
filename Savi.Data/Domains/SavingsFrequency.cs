46e4a580ea34661f5741b0dc2a5f6fab49694476
Ifedayo <ifemicheal2@gmail.com>
2023-10-30 09:26:28 +0100

    Savi backend for hackerton

---
+++ Savi.Data/Domains/SavingsFrequency.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Savi.Data.Domains
+{
+    
+    public class SavingsFrequency
+	{
+        
+        public int Id { get; set; }
+        
+        public string FrequencyName { get; set; }
+
+		public DateTime CreatedAt { get; set; }
+	}
+}
